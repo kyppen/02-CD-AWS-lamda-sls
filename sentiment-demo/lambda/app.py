1af606e68e39dbb7a7341fc64a0958fe3bf58010
Kyppen <fagerhus.sondre@gmail.com>
2024-09-20 09:04:56 +0000

    tigger workflow

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -6,6 +6,7 @@ def handler(event, context):
 
     client = boto3.client('comprehend')
     body = event["body"]
+    console.log("small change to trigger workflow")
     sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
     return {
             "statusCode": 200,
